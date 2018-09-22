2398b2316ab064972bd31e1bc0eba44b272e207d
Dan <danjik7@gmail.com>
2018-09-22 13:45:07 +0300

    Это наш первый комит

---
+++ 1_0/js.js
@@ -0,0 +1,36 @@
+var money = "Ваш бюджет?";
+var magazin = "Название вашего магазина?";
+
+console.log(money);
+console.log(magazin);
+
+
+var mainlist = {};
+
+
+mainList = {
+	budget: 150/30,
+	name_magazin: "Skar",
+	open : true
+	
+
+}
+
+var arr = ['tabble', 'chair', 'sofa']
+
+var employers = {};
+
+employers = {};
+
+var tovar = "Какой тип товаров будем продавать?";
+console.log(tovar);
+
+console.log(arr[0]);
+console.log(tovar);
+
+console.log(arr[1]);
+console.log(tovar);
+
+console.log(arr[2]);
+
+console.log("Budget - " + mainList["budget"]);
---
+++ 1_1/js.js
@@ -0,0 +1,3 @@
+var num = 33721*33721;
+console.log(num);
+alert(num**3);
