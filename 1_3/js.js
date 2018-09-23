d45e1d97b1a75f54094366edb8f4f07033c9c641
Dan <danjik7@gmail.com>
2018-09-23 15:57:31 +0300

    Наш четвертый коммит

---
+++ 1_3/js.js
@@ -0,0 +1,10 @@
+
+var Week = ['monday\n' + 'thursday\n' + 'wednesday\n' + 'tuesday\n' + 'friday\n' + 'saturday\n' + 'sunday'];
+alert(Week);
+
+var arr = ['322', '122', '144', '152', '777', '121', '765'];
+for (var i = 0; i < arr.length; i++) {
+	if (arr[i][0] == '3' || arr[i][0] == '7') {
+		alert(arr[i]);
+	}
+}
