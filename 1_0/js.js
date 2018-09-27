7d31a630bf31adb738a42d988e9276ec24c2e26d
Dan <danjik7@gmail.com>
2018-09-27 08:15:41 +0300

    Наш первый коммит

--- 1_0/js.js
+++ 1_0/js.js
@@ -1,30 +1,50 @@
-var money = "Ваш бюджет?";
-var magazin = "Название вашего магазина?";
-let time = 19;
+let money,
+	name,
+	time,
+	price,
+	dn_budget 
 
-console.log(money);
-console.log(magazin);
+function start() {
+	 money = prompt("Ваш бюджет?");
 
-let mainList = {
-	budget: 150,
-	name_magazin: "Skar",
-	open : true,
-	shopGoods: [],
-	employers: {},
+	 while (isNaN(money) || money == "" || money == null) {
+	 	money = prompt("Ваш бюджет?");
+	 }
+
+	 name = prompt("Название вашего магазина?").toUpperCase();
+	 time = 21;
 }
 
-for (let i = 0; i < 5; i++) {
+//start();
+
+
+let mainList = {
+	budget: 70000,
+	name_magazin: name,
+	open : false,
+	shopGoods: [],
+	employers: {},
+	discount: true
+}
+
+function chooseGoods() {
+	for (let i = 0; i < 5; i++) {
 	let a = prompt("Какой тип товаров будем продавать?");
 	
-	if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50){
+	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50){
 		console.log('All good!');
 		mainList.shopGoods[i] = a;
 	} else {
-
+		i = i - 1;
 	}
+	}	
 }
 
-if (time < 0) {
+//chooseGoods();
+
+
+function workTime(time) {
+	if (time < 0) {
 	console.log('Такого просто не может быть');
 } else if(time > 8 && time < 20){
 	console.log('Время работать!')
@@ -34,6 +54,33 @@ if (time < 0) {
 		console.log('В сутках только 24 часа!');
 	};
 
+}
+workTime(18);
+
+function dnevnoyBudget () {
+	alert("Ежедневный бюджет составляет - " + mainList.budget / 30 );
+}
+
+//dnevnoyBudget();
+
+function discontSystem() {
+	price = 1000;
+	if(mainList.discount == true) {
+		price = (price/100)*80;
+	}
+	console.log("Скидка - " + price + "грн");
+}
+
+//discontSystem();
+
+function hireEmployers() {
+	for (let i = 0; i < 4; i++) {
+		let name = prompt("Имя сотрудника?");
+		mainList.employers[i] = name;
+	}
+}
+
+hireEmployers();
 
 alert(mainList.budget/30);
 
---
+++ 1_4/js.js
@@ -0,0 +1,21 @@
+let num = 20;
+
+function showFirstMessage(text) {
+	alert(text);
+	num = 10;
+}
+
+showFirstMessage("Hello World!");
+console.log(num);
+
+let calc = (a,b) => a+b
+
+console.log(calc(4,5));
+console.log(calc(4,15));
+
+
+
+let str = "12.2px";
+
+console.log(parseInt(str));
+console.log(parseInt(str));
